a481e5fc9f8902de56b2a5558ebad8d384950dd7
Ginguare-hub <ginguare@gmail.com>
2025-10-04 22:47:43 +0300

    C++ Lan 2_1

---
+++ Lab2/Lab2_1/C++/Lab2_1 (C++).cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int Main() 
+{
+
+    typedef double tArr;
+
+    tArr* arr; 
+    int length, i, answer;
+    tArr num, minDiff, absDiff, sumOfNums, arithmMean;
+    
+    sumOfNums = 0;
+    minDiff = 0;
+
+    cin >> length;
+
+    arr = new tArr[length];
+
+    for (i = 0; i < length; i++) 
+    {
+        cout << "Element " << i + 1 << " = ";
+        cin >> num;
+        arr[i] = num;
+        sumOfNums = sumOfNums + num;
+    } 
+
+    arithmMean = sumOfNums / length;
+    minDiff = abs(arithmMean - arr[0]);
+    absDiff = arr[0];
+
+    for (i = 0; i < length; i++)
+    {
+        absDiff = abs(arithmMean - arr[i]);
+
+        if (minDiff > absDiff) 
+        {
+            minDiff = absDiff;
+            answer = i + 1;
+        }
+    }
+
+    cout << "The element " << answer << " in the list is the number, that most fits with the arithmetic mean of the series." << endl;
+
+    delete[] arr;
+    return 0;
+}
