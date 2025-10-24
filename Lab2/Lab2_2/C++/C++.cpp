6fc872f16d534a5824eed33c919c7f5ed01c68a9
Ginguare-hub <ginguare@gmail.com>
2025-10-24 23:30:07 +0300

    2_2

--- Lab2/Lab2_2/C++/C++.cpp
+++ Lab2/Lab2_2/C++/C++.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 
+using namespace std;
+
+bool discoverIsNumberPrime(int number) 
+{
+    int i = 2;
+    bool isShouldStop = false;
+    bool isNumberPrime = false;
+
+    for (i = 2; i < number; i++) 
+    {
+        if  ((number % i == 0) && (!isShouldStop))
+        {
+            isNumberPrime = false;
+            isShouldStop = true;
+        } 
+        else if (!isShouldStop) 
+            isNumberPrime = true;
+    }
+
+    if (number == 2) 
+        isNumberPrime = true;
+
+    return isNumberPrime;
+}
+
+
+
 int main()
 {
+    
+    cout << discoverIsNumberPrime(3);
+    
+    
     return 0;
 }
