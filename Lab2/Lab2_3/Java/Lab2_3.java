092a7acbb71b32219af223423434f0856e094e60
Ginguare-hub <ginguare@gmail.com>
2025-10-26 01:18:14 +0300

    2_3 J

--- Lab2/Lab2_3/Java/Lab2_3.java
+++ Lab2/Lab2_3/Java/Lab2_3.java
@@ -1,8 +1,42 @@
-import System.util.Scanner;
+import java.util.Scanner;
 
 public class Lab2_3 {
 
-    public static main(String[] args) {
+    int readAndVerify(final int MIN_NUMBER, final int MAX_NUMBER, String str) {
+
+        boolean isIncorrect;
+        int num;
+
+        num = 0;
+        isIncorrect = true;
+
+        Scanner scnr = new Scanner(System.in);
+
+        do {
+
+            System.out.print(str);
+            isIncorrect = false;
+
+            try {
+                num = Integer.parseInt(scnr.nextLine());
+            } catch (NumberFormatException e) {
+                isIncorrect = true;
+                System.out.println("Incorrect input, try again.");
+            }
+
+            if (!isIncorrect && (num < MIN_NUMBER || num > MAX_NUMBER)) {
+                isIncorrect = true;
+                System.out.printf("The number must fit the range [%d,%d].\n", MIN_NUMBER, MAX_NUMBER);
+            }
+
+        } while (isIncorrect);
+
+        scnr.close();
+
+        return num;
+    }
+
+    public static void main(String[] args) {
 
     }
 }
