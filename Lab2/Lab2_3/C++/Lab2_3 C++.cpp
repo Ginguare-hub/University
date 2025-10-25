3eacb1c3d9e1453bf703488672ba0afa2dd2cef6
Ginguare-hub <ginguare@gmail.com>
2025-10-25 22:59:41 +0300

    2_3

---
+++ Lab2/Lab2_3/C++/Lab2_3 C++.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int** writeInMatrix(int n, int** arrayA)
+{
+
+    arrayA = new double*[n];
+
+    
+
+}
+
+int main()
+{
+
+    double** a;
+
+
+
+
+
+
+    
+}
